363fa2397cc9f3f35c5a436f857776f0e1c66f00
KawaharaRina <cyokodaigorou@gmail.com>
2025-03-21 16:17:14 +0900

    チュートリアルパネル・リザルトパネル等追加

--- PingPongDash/Assets/Scripts/Common.cs
+++ PingPongDash/Assets/Scripts/Common.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Common : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 定数
 
-    // Update is called once per frame
-    void Update()
+    // シーン名
+    public const string SCENE_TITLE = "Title";
+    public const string SCENE_GAME = "UIScene";
+    public const string SCENE_STAGE1 = "Stage1";
+    public const string SCENE_STAGE2 = "Stage2";
+
+    /// <summary>
+    /// シーンを呼び出す汎用関数
+    /// </summary>
+    static public void LoadScene(string _sceneName)
     {
-        
+        SceneManager.LoadScene(_sceneName);
     }
 }
---
+++ PingPongDash/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // チュートリアルパネルのスライドアニメーション用
+    SlideAnimation slideAnimation;
+    private bool isSlide;
+
+    // リザルトパネルのズームアニメーション用
+    ZoomAnimation zoomAnimation;
+
+    // フェードプレファブ取得用
+    FadeAnimation fadeAnimation;
+
+    // シーンする遷移格納用
+    private string sceneName;
+    
+
+    /// <summary>
+    /// 初期化関数
+    /// </summary>
+    private void Init()
+    {
+        // チュートリアルパネル取得
+        slideAnimation = GameObject.Find("TutorialPanel").GetComponent<SlideAnimation>();
+        isSlide = false;
+
+        // リザルトパネル取得
+        zoomAnimation = GameObject.Find("ResultPanel").GetComponent<ZoomAnimation>();
+
+        // フェードプレファブ取得
+        fadeAnimation = GameObject.Find("FadePrefab").GetComponent<FadeAnimation>();
+        
+    }
+
+    /// <summary>
+    /// タイトルボタン押下時
+    /// </summary>
+    public void PushTitleButton()
+    {
+        sceneName = Common.SCENE_TITLE;
+    }
+
+    /// <summary>
+    /// リトライボタン押下時
+    /// </summary>
+    public void PushRetryButton()
+    {
+        sceneName = Common.SCENE_GAME;
+    }
+
+    /// <summary>
+    /// チュートリアルパネルをスライドイン
+    /// </summary>
+    private void ShowTutorialPanel()
+    {
+        // フェードインが終わってからスライドイン
+        if (fadeAnimation.GetAlpha() <= 0 && !isSlide)
+        {
+            slideAnimation.SlideIn();
+            isSlide = true;
+        }
+    }
+
+    /// <summary>
+    /// リザルトパネルをズームイン
+    /// </summary>
+    private void ShowResultPanel()
+    {
+        // TODO リザルトを出す条件の変更
+        // タイムアップ？大家さんに見つかったとき？
+        if (Input.GetKey(KeyCode.A))
+        {
+            zoomAnimation.ShowZoomIn();
+        }
+    }
+
+    /// <summary>
+    /// シーンを切り替える処理
+    /// </summary>
+    private void SceneChange()
+    {
+        if (fadeAnimation.GetAlpha() >= 1.0f)
+        {
+            switch (sceneName)
+            {
+                case Common.SCENE_TITLE:
+                    Common.LoadScene(Common.SCENE_TITLE);
+                    break;
+
+                case Common.SCENE_GAME:
+                    Common.LoadScene(Common.SCENE_GAME);
+                    break;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        // 初期化処理
+        Init();
+    }
+
+    private void Update()
+    {
+        // チュートリアルパネルスライドイン
+        ShowTutorialPanel();
+
+        // リザルトパネルズームイン
+        ShowResultPanel();
+
+        // シーン変更処理
+        SceneChange();
+    }
+
+}
