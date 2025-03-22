c894b7f442f4a867ea517a6af5c62b09de0e99bf
KabasawaHinata <kabasawahinata0201@gmail.com>
2025-03-22 10:11:29 +0900

    Mouseの上下移動の監視クラスの作成

---
+++ PingPongDash/Assets/Scripts/Input/MouseMoveDirection.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+namespace Kabasawa
+{
+    public enum MouseMoveDirection
+    {
+        NONE,
+        UP,
+        DOWN,
+    }
+
+}
+
