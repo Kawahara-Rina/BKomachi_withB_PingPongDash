bd9a3f62a4915535304190dc46d6997b7f98107d
KabasawaHinata <kabasawahinata0201@gmail.com>
2025-03-23 10:54:14 +0900

    EnemyScriptの作成

---
+++ PingPongDash/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //来る頻度の最大値
+    float MAX_SPAN = 25;
+
+    //ドアまでの距離
+    float distanece;
+
+    //足音が聞こえ始める距離
+    float audioActiveDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
